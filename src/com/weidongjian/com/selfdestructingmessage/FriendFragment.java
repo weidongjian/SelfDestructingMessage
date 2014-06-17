3c17548da2eb668c2078ed64273fef688eea339f
weidongjian <weidongjian@gmail.com>
2014-06-17 22:18:02 +0800

    add SectionPagerAdapter
    add SectionPagerAdapter, InboxFragment, FriendFragment

---
+++ src/com/weidongjian/com/selfdestructingmessage/FriendFragment.java
@@ -0,0 +1,16 @@
+package com.weidongjian.com.selfdestructingmessage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FriendFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_friend, container, false);
+		return rootView;
+	}
+}
---
+++ src/com/weidongjian/com/selfdestructingmessage/SectionsPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.weidongjian.com.selfdestructingmessage;
+
+import java.util.Locale;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	
+	protected Context mContext;
+
+	public SectionsPagerAdapter(Context context, FragmentManager fm) {
+		super(fm);
+		mContext = context;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		switch (position) {
+		case 0:
+			return new InboxFragment();
+		case 1:
+			return new FriendFragment(); 
+		}
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// Show 3 total pages.
+		return 2;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		Locale l = Locale.getDefault();
+		switch (position) {
+		case 0:
+			return mContext.getString(R.string.title_section1).toUpperCase(l);
+		case 1:
+			return mContext.getString(R.string.title_section2).toUpperCase(l);
+		}
+		return null;
+	}
+
+}
