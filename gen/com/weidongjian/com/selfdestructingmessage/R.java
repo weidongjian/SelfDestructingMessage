8b26e706f9f330f17db03c4e751f01f25446e667
weidongjian <weidongjian@gmail.com>
2014-06-17 23:20:10 +0800

    config login layout

--- gen/com/weidongjian/com/selfdestructingmessage/R.java
+++ gen/com/weidongjian/com/selfdestructingmessage/R.java
@@ -12,6 +12,8 @@ public final class R {
     }
     public static final class color {
         public static final int Text=0x7f040000;
+        public static final int background_activity=0x7f040001;
+        public static final int background_editText=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -56,6 +58,7 @@ public final class R {
         public static final int action_settings=0x7f060004;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060006;
+        public static final int hint_password=0x7f060010;
         public static final int hint_username=0x7f06000c;
         public static final int login=0x7f06000a;
         public static final int login_subtilte=0x7f06000b;
