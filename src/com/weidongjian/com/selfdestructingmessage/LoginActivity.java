09142041a3291399fdc4eda70f05634c6014f104
weidongjian <weidongjian@gmail.com>
2014-06-18 22:45:44 +0800

    config show password check box

--- src/com/weidongjian/com/selfdestructingmessage/LoginActivity.java
+++ src/com/weidongjian/com/selfdestructingmessage/LoginActivity.java
@@ -2,21 +2,78 @@ package com.weidongjian.com.selfdestructingmessage;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Build;
 
 public class LoginActivity extends Activity {
+	
+	protected EditText mUsername;
+	protected EditText mPassword;
+	protected CheckBox mShowPasswordCheckBox;
+	protected Button mLoginButton;
+	protected TextView mSingupTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		
+		mUsername = (EditText) findViewById(R.id.et_username);
+		mPassword = (EditText) findViewById(R.id.et_password);
+		mShowPasswordCheckBox = (CheckBox) findViewById(R.id.cb_show_password);
+		mLoginButton = (Button) findViewById(R.id.bt_login);
+		mSingupTextView = (TextView) findViewById(R.id.tv_singup);
+
+		mLoginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String username = mUsername.getText().toString().trim();
+				String password = mPassword.getText().toString().trim();
+				
+				if (username.isEmpty() || password.isEmpty()) {
+					new AlertDialog.Builder(LoginActivity.this)
+					.setTitle("Oops!")
+					.setMessage("Please make sure you have inputed the username and password.")
+					.setPositiveButton(android.R.string.ok, null)
+					.show();
+				}
+				
+			}
+		});
+		
+		mShowPasswordCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					mPassword.setTransformationMethod(null);
+					mPassword.setSelection(mPassword.getText().length());
+				}
+				else {
+					mPassword.setTransformationMethod(new PasswordTransformationMethod());
+					mPassword.setSelection(mPassword.getText().length());
+				}
+					
+				
+			}
+		});
 
 	}
 
