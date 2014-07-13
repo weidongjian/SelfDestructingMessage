4689bbc7402e7232f00768d96f6cb21bf5c31d9d
weidongjian <weidongjian@gmail.com>
2014-07-13 11:49:41 +0800

    backup

--- gen/eu/erikw/R.java
+++ gen/eu/erikw/R.java
@@ -8,7 +8,7 @@ package eu.erikw;
 
 public final class R {
 	public static final class drawable {
-		public static final int ptr_pulltorefresh_arrow = 0x7f020022;
+		public static final int ptr_pulltorefresh_arrow = 0x7f020023;
 	}
 	public static final class id {
 		public static final int ptr_id_header = 0x7f0c0011;
